4ce3cce94df1aaaae5efb29f1566224056b07f1d
iroha999 <gongen4090@icloud.com>
2025-01-08 15:41:03 +0900

    github再アップ

--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,14 @@ export default function Home() {
       <Link href="/dashboard" className="w-64 text-center py-2 px-4 bg-black text-white rounded hover:bg-gray-800 transition-colors">
         始める
       </Link>
-      <div className="mt-8">
+      {/* <div className="mt-8">
         <Link href="/api/helloNextJs" className="block text-center py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-700 transition-colors mb-4">
           Hello NextJS
         </Link>
         <Link href="/api/py/helloFastApi" className="block text-center py-2 px-4 bg-green-500 text-white rounded hover:bg-green-700 transition-colors">
           Hello FastAPI
         </Link>
-      </div>
+      </div> */}
     </div>
   )
 }
